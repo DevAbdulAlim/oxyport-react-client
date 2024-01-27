bd0d136cd92aa4f18379d60b41bdea218b2a60ea
Abdul Alim <aa.abdulalim13@gmail.com>
2024-01-27 19:38:27 +0600

    updated admin side nav

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -1,14 +1,30 @@
 // AdminLayout.tsx
 
-import React from "react";
+import React, { useState } from "react";
 import AdminSidebar from "../../sections/AdminSidebar";
 import { Outlet } from "react-router-dom";
+import Button from "../../components/Button";
 
 const AdminLayout: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleNavToggle = () => {
+    setOpen(!open);
+  };
   return (
     <div className="flex flex-col h-screen">
-      <AdminSidebar />
-      <div className="flex-1 p-5 ml-64 overflow-x-hidden overflow-y-auto ">
+      <AdminSidebar open={open} handleNavToggle={handleNavToggle} />
+      <div
+        className={`flex-1 overflow-x-hidden overflow-y-auto ${
+          open ? "xl:ml-64" : "xl:ml-16"
+        } `}
+      >
+        <div className="flex justify-between py-4 mb-8 text-gray-800 bg-gray-200 shadow-md">
+          <Button onClick={handleNavToggle} className="xl:hidden">
+            Toggle
+          </Button>
+          TopBar
+        </div>
         <Outlet />
       </div>
     </div>
--- src/sections/AdminSidebar.tsx
+++ src/sections/AdminSidebar.tsx
@@ -1,52 +1,121 @@
-// AdminSidebar.tsx
-
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../components/Button";
+import { IoIosHome, IoIosListBox, IoIosCart, IoMdPeople } from "react-icons/io";
 import { useAuth } from "../context/AuthContext";
+import { FiLogOut } from "react-icons/fi";
+import { GiMoebiusTriangle } from "react-icons/gi";
+import { ImMenu } from "react-icons/im";
+import Button from "../components/Button";
 
-const AdminSidebar: React.FC = () => {
+const AdminSidebar = ({
+  open,
+  handleNavToggle,
+}: {
+  open: boolean;
+  handleNavToggle: () => void;
+}) => {
   const { logout } = useAuth();
 
   const handleLogout = async () => {
-    await logout();
+    logout();
   };
 
   return (
-    <nav className="fixed left-0 w-64 h-full p-4 text-white bg-gray-800">
-      <div className="mb-4 text-2xl font-semibold">Admin Dashboard</div>
-
+    <nav
+      className={`fixed   left-0 h-full p-4 group hover:w-64 overflow-hidden text-white bg-gray-800 ${
+        open ? "w-64 block" : "xl:w-16 hidden xl:block"
+      }`}
+    >
+      <div className="flex space-x-4">
+        <GiMoebiusTriangle className="text-4xl" />
+        <span
+          className={`${
+            open ? "block" : "hidden"
+          } mb-8 group-hover:block text-2xl font-semibold`}
+        >
+          ActivEco
+        </span>
+      </div>
+      <Button
+        onClick={handleNavToggle}
+        variant="ghost"
+        className={` absolute  group-hover:block top-2 right-2 ${
+          open ? "block bg-gray-700" : "xl:hidden"
+        }`}
+      >
+        <ImMenu />
+      </Button>
       <ul>
-        <li className="mb-2">
-          <Link to="/admin" className="text-gray-300 hover:text-white">
+        <li className="flex items-center mb-4">
+          <IoIosHome className="text-2xl" />
+          <Link
+            to="/admin"
+            className={`${
+              open ? "block" : "hidden"
+            } text-gray-300 group-hover:block ml-2 hover:text-white`}
+          >
             Dashboard
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="flex items-center mb-4">
+          <IoIosListBox className="text-2xl" />
           <Link
             to="/admin/categories"
-            className="text-gray-300 hover:text-white"
+            className={`${
+              open ? "block" : "hidden"
+            } text-gray-300 group-hover:block ml-2 hover:text-white`}
           >
             Categories
           </Link>
         </li>
-        <li className="mb-2">
-          <Link to="/admin/products" className="text-gray-300 hover:text-white">
+        <li className="flex items-center mb-4">
+          <IoIosCart className="text-2xl" />
+          <Link
+            to="/admin/products"
+            className={`${
+              open ? "block" : "hidden"
+            } text-gray-300 group-hover:block ml-2 hover:text-white`}
+          >
             Products
           </Link>
         </li>
-        <li className="mb-2">
-          <Link to="/admin/orders" className="text-gray-300 hover:text-white">
+        <li className="flex items-center mb-4">
+          <IoIosCart className="text-2xl" />
+          <Link
+            to="/admin/orders"
+            className={`${
+              open ? "block" : "hidden"
+            } text-gray-300 group-hover:block ml-2 hover:text-white`}
+          >
             Orders
           </Link>
         </li>
-        <li className="mb-2">
-          <Link to="/admin/users" className="text-gray-300 hover:text-white">
+        <li className="flex items-center mb-4">
+          <IoMdPeople className="text-2xl" />
+          <Link
+            to="/admin/users"
+            className={`${
+              open ? "block" : "hidden"
+            } text-gray-300 group-hover:block ml-2 hover:text-white`}
+          >
             Users
           </Link>
         </li>
-        <Button onClick={handleLogout}>Logout</Button>
       </ul>
+
+      <Button
+        variant="ghost"
+        onClick={handleLogout}
+        className="absolute p-0 hover:p-2 bottom-4"
+      >
+        <FiLogOut className="text-2xl" />
+        <span
+          className={`${open ? "block" : "hidden"} ml-2 group-hover:block `}
+        >
+          {" "}
+          Logout
+        </span>
+      </Button>
     </nav>
   );
 };
